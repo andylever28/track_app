af794cb7b58eff0d352f6eb11e6fc437ba8e0d11
Andrew Lever <andylever28@gmail.com>
2018-03-10 11:10:52 -0500

    move app component to own folder. swap css for sass

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
 import { HomepageComponent } from './homepage/homepage.component'
 import { LoginComponent } from './login/login.component'
 import { SignupComponent } from './signup/signup.component'
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
